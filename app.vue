c670c6d83642a5f44740e4be675c1173a6149df6
AndreySamaev <andreysamaev@gmail.com>
2025-07-23 20:25:20 +0300

    fix layouts app

---
+++ app.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+  <NuxtLayout>
+   <NuxtPage />
+  </NuxtLayout>
+ </div>
+</template>
+
+<style>
+body {
+ margin: 0;
+ padding: 0;
+}
+a {
+ text-decoration: none;
+}
+</style>
+<script setup lang="ts">
+</script>
